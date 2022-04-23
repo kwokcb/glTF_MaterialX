257bed01ca8417a6600c5916074f69e1347e0010
kwokcb <kwokcb@gmail.com>
2022-04-22 22:18:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ cd build
 cmake -DMATERIALX_ROOT="/Users/bernardkwok/work/bernard_materialx/build/installed" -GXcode -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 cmake --build . --target install --config RelWithDebInfo
 cd ..
-build/bin/RelWithDebInfo/glTF2Mtlx --gltf /Users/bernardkwok/work/glTF-Sample-Models/2.0/BoomBox/glTF/BoomBox.gltf --lib /Users/bernardkwok/work/bernard_materialx
-less /Users/bernardkwok/work/glTF-Sample-Models/2.0/BoomBox/glTF/BoomBox.gltf.mtlx
+build/bin/RelWithDebInfo/glTF2Mtlx --gltf build/bin/resources/gltf/BoomBoxWithAxes.gltf --lib /Users/bernardkwok/work/bernard_materialx
+build/bin/RelWithDebInfo/glTF2Mtlx --mtlx build/bin/resources/gltf/BoomBoxWithAxes.gltf.mtlx --lib /Users/bernardkwok/work/bernard_materialx
+
+cat build/bin/resources/gltf/BoomBoxWithAxes.gltf.mtlx
+cat build/bin/resources/gltf/BoomBoxWithAxes.gltf.mtlx._converted.gltf
+
 
 
