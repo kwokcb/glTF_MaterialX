4da3640eb33ee418e1cafe916bf70529b837333b
Bernard Kwok <kwokcb@gmail.com>
2022-12-09 14:38:48 -0500

    Fix export dir + logging.

--- source/glTFMtlxTest/MaterialXglTF/MaterialXglTF.cpp
+++ source/glTFMtlxTest/MaterialXglTF/MaterialXglTF.cpp
@@ -63,7 +63,7 @@ bool haveSingleDocBake(const mx::FilePath& errorFile)
 }
 
 bool bakeDocument(const mx::FilePath& inputFileName, const mx::FilePath& outputFilename, 
-                     unsigned int width, unsigned int height)
+                     unsigned int width, unsigned int height, std::ostream& logFile)
 {
     // Run test renders on output
     mx::FilePath shaderTranslator(MTLX_TRANSLATE_SHADER);
@@ -82,7 +82,7 @@ bool bakeDocument(const mx::FilePath& inputFileName, const mx::FilePath& outputF
             + " " + outputFilename.asString()
             + " > " + errorFile + redirectString;
 
-        std::cout << "- Translated MTLX: " + outputFilename.asString() << std::endl;
+        logFile << "- Translated MTLX: " + outputFilename.asString() << std::endl;
         int returnValue = std::system(command.c_str());
 
         std::ifstream errorStream(errorFile);
@@ -94,10 +94,10 @@ bool bakeDocument(const mx::FilePath& inputFileName, const mx::FilePath& outputF
         if (renderError)
         {
             mx::StringVec errors;
-            std::cout << "- Errors: " << std::endl;
-            std::cout << "  - Command string : " + command << std::endl;
-            std::cout << "  - Command return code: " + std::to_string(returnValue) << std::endl;
-            std::cout << "  - Log: " << result << std::endl;
+            logFile << "- Errors: " << std::endl;
+            logFile << "  - Command string : " + command << std::endl;
+            logFile << "  - Command return code: " + std::to_string(returnValue) << std::endl;
+            logFile << "  - Log: " << result << std::endl;
         }
         return true;
         //CHECK(!renderError);
@@ -108,6 +108,9 @@ bool bakeDocument(const mx::FilePath& inputFileName, const mx::FilePath& outputF
 // MaterialX to glTF to MaterialX tests
 TEST_CASE("Validate export", "[gltf]")
 {
+    std::ofstream logFile;
+    logFile.open("gltf_export_log.txt");
+
     const std::string GLTF_EXTENSION("gltf");
     const std::string MTLX_EXTENSION("mtlx");
 
@@ -133,11 +136,11 @@ TEST_CASE("Validate export", "[gltf]")
     if (!rootPath.isEmpty())
     {
         useExternalPath = true;
-        std::cout << "MaterialX export directory used: " << rootPath.asString() << std::endl;
+        logFile << "MaterialX export directory used: " << rootPath.asString() << std::endl;
     }
     else
     {
-        rootPath = "resources/glTF_export/Materials/Examples";
+        rootPath = "resources/glTF_export/Materials/Examples/StandardSurface";
         rootPath = searchPath.find(rootPath);
     }
 
@@ -152,9 +155,11 @@ TEST_CASE("Validate export", "[gltf]")
                 std::string::npos != fullPath.asString().find("distilled") ||
                 std::string::npos != fullPath.asString().find("baked"))
             {
-                //std::cout << "------------ Skip file: " << fullPath.asString() << std::endl;
+                //logFile << "------------ Skip file: " << fullPath.asString() << std::endl;
                 continue;
             }
+
+            logFile << "* Convert from MaterialX to gltF: " << fullPath.asString() << std::endl;
 
             mx::DocumentPtr doc = mx::createDocument();
             doc->importLibrary(libraries);
@@ -170,16 +175,17 @@ TEST_CASE("Validate export", "[gltf]")
             // Perform shader translation in place
             const std::string distilledFileName = fileName.asString() + "_distilled.mtlx";
             gltfMTLXLoader->translateShaders(doc);
-            std::cout << "- Wrote distilled file : " << distilledFileName << std::endl;
+            logFile << "  * Wrote distilled file : " << distilledFileName << std::endl;
             mx::writeToXmlFile(doc, distilledFileName, &writeOptions);
             testedFiles.insert(distilledFileName);
 
             // Bake to a new document
             const mx::FilePath bakedFileName = fileName.asString() + "_baked.mtlx";
-            bakeDocument(distilledFileName, bakedFileName, 512, 512);
+            bakeDocument(distilledFileName, bakedFileName, 512, 512, logFile);
             testedFiles.insert(bakedFileName);
             if (bakedFileName.exists())
             {
+                logFile << "  * Created bake file: " << bakedFileName.asString() << std::endl;
                 mx::DocumentPtr bakedDoc = mx::createDocument();
                 bakedDoc->importLibrary(libraries);
                 readFromXmlFile(bakedDoc, bakedFileName);
@@ -189,16 +195,29 @@ TEST_CASE("Validate export", "[gltf]")
                 bool convertedToGLTF = mtlx2glTF(gltfMTLXLoader, outputFileName, bakedDoc);
                 if (convertedToGLTF)
                 {
-                    std::cout << "Converted to gltf: " << outputFileName << std::endl;
+                    logFile << "  * Converted to gltf: " << outputFileName << std::endl;
                 }
+                else
+                {
+                    logFile << "  * Failed to convert to gltf: " << outputFileName << std::endl;
+                }
+            }
+            else
+            {
+                logFile << "  * Failed to bake: " << bakedFileName.asString() << std::endl;
             }
         }
     }
+
+    logFile.close();
 }
 
 // glTF to MaterialX to glTF tests
 TEST_CASE("Validate import", "[gltf]")
 {
+    std::ofstream logFile;
+    logFile.open("gltf_import_log.txt");
+
     const std::string GLTF_EXTENSION("gltf");
     const std::string MTLX_EXTENSION("mtlx");
 
@@ -225,7 +244,7 @@ TEST_CASE("Validate import", "[gltf]")
     if (!rootPath.isEmpty())
     {
         useSampleModels = true;
-        std::cout << "glTF sample models directory used: " << rootPath.asString() << std::endl;
+        logFile << "glTF sample models directory used: " << rootPath.asString() << std::endl;
     }
     else
     {
@@ -239,7 +258,8 @@ TEST_CASE("Validate import", "[gltf]")
     }
     if (!rootPath.exists())
     {
-        std::cout << "Test glTF directory not found: " << rootPath.asString() << ". Skipping test" << std::endl;
+        logFile << "Test glTF directory not found: " << rootPath.asString() << ". Skipping test" << std::endl;
+        logFile.close();
         return;
     }
 
@@ -280,15 +300,15 @@ TEST_CASE("Validate import", "[gltf]")
                     std::string message;
                     if (!materials->validate(&message))
                     {
-                        std::cerr << "- Validation warnings document created from: " << fullPath.asString() << " ***" << std::endl;
-                        std::cerr << message;
+                        logFile << "- Validation warnings document created from: " << fullPath.asString() << " ***" << std::endl;
+                        logFile << message;
                     }
                     mx::FilePath fileName = fullPath.getBaseName();
                     fileName.removeExtension();
                     fileName = dir / fileName;
 
                     const std::string outputFileName = fileName.asString() + "_fromgltf.mtlx";
-                    std::cout << "- Wrote " << std::to_string(nodes.size()) << " materials to file : " << outputFileName << std::endl;
+                    logFile << "- Wrote " << std::to_string(nodes.size()) << " materials to file : " << outputFileName << std::endl;
                     mx::writeToXmlFile(materials, outputFileName, &writeOptions);
 
                     mx::MaterialHandlerPtr gltfMTLXLoader = mx::GltfMaterialHandler::create();
@@ -298,7 +318,7 @@ TEST_CASE("Validate import", "[gltf]")
                     if (convertedToGLTF)
                     {
                         testedFiles.insert(outputFileName2);
-                        std::cout << "- Wrote MTLX materials to glTF file : " << outputFileName2 << std::endl;
+                        logFile << "- Wrote MTLX materials to glTF file : " << outputFileName2 << std::endl;
                     }
 
                     // Run test renders on output
@@ -318,7 +338,7 @@ TEST_CASE("Validate import", "[gltf]")
                             + " --envSampleCount 4"
                             + " > " + errorFile + redirectString;
 
-                        std::cout << "- Render image: " + imageFileName << std::endl;
+                        logFile << "- Render image: " + imageFileName << std::endl;
                         int returnValue = std::system(command.c_str());
 
                         std::ifstream errorStream(errorFile);
@@ -330,10 +350,10 @@ TEST_CASE("Validate import", "[gltf]")
                         if (renderError)
                         {
                             mx::StringVec errors;
-                            std::cout << "- Errors: " << std::endl;
-                            std::cout << "  - Command string : " + command << std::endl;
-                            std::cout << "  - Command return code: " + std::to_string(returnValue) << std::endl;
-                            std::cout << "  - Log: " << result << std::endl;
+                            logFile << "- Errors: " << std::endl;
+                            logFile << "  - Command string : " + command << std::endl;
+                            logFile << "  - Command return code: " + std::to_string(returnValue) << std::endl;
+                            logFile << "  - Log: " << result << std::endl;
                         }
                         CHECK(!renderError);
                     }
@@ -341,4 +361,6 @@ TEST_CASE("Validate import", "[gltf]")
             }
         }
     }
+
+    logFile.close();
 }
